e622bbac69cbc93946db186dbd04f4b3ca5c6b82
antonvh <antonhaninvl@gmail.com>
2022-12-20 11:01:20 +0300

    добавил поддержку tailwindcss

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 
 const Footer: FC = () => {
-  return (null);
+  return (
+    <div className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/TenderPage.tsx
+++ src/pages/TenderPage.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from 'react';
 
+
 const TenderPage: FC = () => {
-  return (null);
+
+  return (
+    <div className="flex justify-center mx-auto h-[calc(100vh-100px)]">
+      <div className="flex flex-col max-w-[615px]">
+        <div className="container">
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TenderPage;
+   
