90dc93aa05a216bc4d1f8cc72bf7422131e285c6
antonvh <antonhaninvl@gmail.com>
2022-12-21 12:17:09 +0300

    отступы в Timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, FC, useCallback } from 'react'
+import React, { useState, useEffect, FC } from 'react'
 
 
 type Props = {
@@ -8,38 +8,38 @@ type Props = {
 }
 
 const Timer: FC<Props> = (props) => {
-    const { initialMinute = 0,initialSeconds = 0, callback } = props;
-    const [ minutes, setMinutes ] = useState(initialMinute);
-    const [seconds, setSeconds ] =  useState(initialSeconds);
-    useEffect(() => {
-    let myInterval = setInterval(
-      () => {
-        if (seconds > 0) {
-            setSeconds(seconds - 1);
+  const { initialMinute = 0,initialSeconds = 0, callback } = props;
+  const [ minutes, setMinutes ] = useState(initialMinute);
+  const [seconds, setSeconds ] =  useState(initialSeconds);
+  useEffect(() => {
+  let myInterval = setInterval(
+    () => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+          callback?.();
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
         }
-        if (seconds === 0) {
-            if (minutes === 0) {
-                clearInterval(myInterval);
-                callback?.();
-            } else {
-                setMinutes(minutes - 1);
-                setSeconds(59);
-            }
-        } 
-      }, 1000);
-      return () => {
-        clearInterval(myInterval);
-      };
-    });
+      } 
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
 
-    return (
-      <div>
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
-      </div>
-    )
+  return (
+    <>
+      { minutes === 0 && seconds === 0
+        ? null
+        : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+      }
+    </>
+  )
 }
 
 export default Timer;
